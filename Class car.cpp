bc1f3cc39f6a0a9344649ea565db06a3a38793c2
Kaveri1807 <kaveri.rajguru24@sanjivani.edu.in>
2024-10-08 18:58:52 +0530

    Create Class car.cpp
    This program defines a C++ class "Car" with attributes brand, model, and year.Two "Car" objects, carObj1 and carObj2, are created and initialized.
    The program demonstrates object-oriented programming (OOP) concepts.Attribute values are printed to the console for both car objects.Illustrates basic class definition and object manipulation in C++.

---
+++ Class car.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Car {
+
+public:
+
+string brand;
+string model;
+int year;
+};
+int main() {
+// Create an object of Car
+Car carObj1;
+carObj1.brand = "Toyota";
+carObj1.model = "Corolla";
+carObj1.year = 1956;
+// Create another object of Car
+Car carObj2;
+carObj2.brand = "Honda";
+carObj2.model = "CR-V";
+carObj2.year = 1997;
+// Print attribute values
+cout << carObj1.brand << " " << carObj1.model << " " << carObj1.year << "\n";
+cout << carObj2.brand << " " << carObj2.model << " " << carObj2.year << "\n";
+return 0;
+}
